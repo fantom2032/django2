2faa47da24c125c79d6a629a1b4443db3bd12d8b
Руслан <fantom203232@gmail.com>
2025-05-15 15:19:10 +0500

    Delete views.py

--- views.py
+++
@@ -1,8 +0,0 @@
-from django.views import View
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.hashers import make_password
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, redirect
-from django.contrib import messages
-
-from clients.models import Client
